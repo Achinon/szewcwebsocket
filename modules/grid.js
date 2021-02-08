4f77753412aac0008b0e5fc02fc818a773eebff6
Emil Gutkiewicz <achinonek@gmail.com>
2021-02-08 19:52:23 +0100

    update dupa

--- modules/grid.js
+++
@@ -1,5 +0,0 @@
-const Grid = class {
-    constructor(){
-        
-    }
-}
--- modules/player.js
+++
@@ -1,9 +0,0 @@
-const Player = class {
-    points = 0;
-
-    constructor(nick, color, socket){
-        this.nick = nick;
-        this.color = color;
-        this.socket = socket;
-    }
-}
